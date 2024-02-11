c87f95a00911a0a6ce715a8252861f79d45cafe8
Frangiop <cybergiunti@gmail.com>
2024-02-11 20:04:20 +0100

    first capitulos

--- src/pages/CapituloCas1.js
+++ src/pages/CapituloCas1.js
@@ -6,15 +6,15 @@ const CapituloCas1 = () => {
   return (
     <div>
       <div className="imagenes-adicionales"> {/* Imágenes de abajo */}
-        <img src="/casemiro/cap1/cas1.png" alt="Imagen 1" />
-        <img src="/casemiro/cap1/cas2.png" alt="Imagen 2" />
-        <img src="/casemiro/cap1/cas3.png" alt="Imagen 3" />
-        <img src="/casemiro/cap1/cas4.png" alt="Imagen 4" />
-        <img src="/casemiro/cap1/cas5.png" alt="Imagen 5" />
-        <img src="/casemiro/cap1/cas6.png" alt="Imagen 6" />
-        <img src="/casemiro/cap1/cas7.png" alt="Imagen 7" />
-        <img src="/casemiro/cap1/cas8.png" alt="Imagen 8" />
-        <img src="/casemiro/cap1/cas9.png" alt="Imagen 9" />
+        <img src="/casemiro/capitulo1/cas1.png" alt="Imagen 1" />
+        <img src="/casemiro/capitulo1/cas2.png" alt="Imagen 2" />
+        <img src="/casemiro/capitulo1/cas3.png" alt="Imagen 3" />
+        <img src="/casemiro/capitulo1/cas4.png" alt="Imagen 4" />
+        <img src="/casemiro/capitulo1/cas5.png" alt="Imagen 5" />
+        <img src="/casemiro/capitulo1/cas6.png" alt="Imagen 6" />
+        <img src="/casemiro/capitulo1/cas7.png" alt="Imagen 7" />
+        <img src="/casemiro/capitulo1/cas8.png" alt="Imagen 8" />
+        <img src="/casemiro/capitulo1/cas9.png" alt="Imagen 9" />
       </div>
     </div>
   );
