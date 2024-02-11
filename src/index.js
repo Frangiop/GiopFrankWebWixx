998a63a4c7f810f076bf30fc600842df0f1ea08e
Frangiop <cybergiunti@gmail.com>
2024-02-11 23:47:54 +0100

    images changed root

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('docs'));
 root.render(
 
     <BrowserRouter>
